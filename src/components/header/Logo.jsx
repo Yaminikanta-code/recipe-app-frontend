9fb9962d0b5cd8c0b884b20032974d1cfc1646de
Yaminikanta-code <yaminikantaprof1@gmail.com>
2025-02-11 15:42:17 +0530

    final recipe app

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Logo() {
   return (
     <div className="flex items-center space-x-2">
-      <span className="text-4xl font-bold mx-4 text-primary">RecipeHub</span>
+      <span className="text-2xl sm:text-4xl font-bold mx-4 text-primary">
+        RecipeHub
+      </span>
     </div>
   );
 }
